7452e62e2f06a4fd14bfb524f60ad4c3413f1c97
Sean <sean.yong@hk.ncs-i.com>
2022-09-02 18:36:35 +0800

    updated some art

--- pilexp.py
+++ pilexp.py
@@ -1,26 +1,27 @@
 from PIL import Image, ImageOps
 
-screen_h, screen_w = 700, 1100
-tile_h, tile_w = 105, 192
-camel_h = 96
+screen_h, screen_w = 112, 160
+tile_h, tile_w = 16, 32
+camel_h = 16
+offset = 1
 
-canvas = Image.new("RGBA", (screen_w, screen_h), (34, 32, 52))
-tile = Image.open("graphics/floor_compact.png")
+canvas = Image.new("RGBA", (screen_w, screen_h), (228, 210, 170))
+tile = Image.open("graphics/tilealt.png")
 
 def tile_iso(i, j):
-    x = i*0.52*tile_w + j*-0.52*tile_w + (screen_w/2 - tile_w/2)
-    y = i*0.48*tile_h + j*0.48*tile_h + tile_h*1.5
-    return (int(x), int(y))
+    x = i*0.5*tile_w + j*-0.5*tile_w + (screen_w/2 - tile_w/2)
+    y = i*0.5*tile_h + j*0.5*tile_h + tile_h*1.5 + offset
+    return (round(x), round(y))
 
 def iso_camel(i, j, k=0):
-    x = i*0.52*tile_w + j*-0.52*tile_w + (screen_w/2 - tile_w/4)
-    y = i*0.48*tile_h + j*0.48*tile_h + tile_h*1.25 - k*camel_h/2.2
-    return (int(x), int(y))
+    x = i*0.5*tile_w + j*-0.5*tile_w + (screen_w/2 - tile_w/4)
+    y = i*0.5*tile_h + j*0.5*tile_h + tile_h*1.25 - k*camel_h/2.75 + offset
+    return (round(x), round(y))
 
 def iso_mod(i, j, k=0):
-    x = i*0.52*tile_w + j*-0.52*tile_w + (screen_w/2 - tile_w/4)
-    y = i*0.48*tile_h + j*0.48*tile_h + tile_h*1.25 + 40
-    return (int(x), int(y))
+    x = i*0.5*tile_w + j*-0.5*tile_w + (screen_w/2 - tile_w/4)
+    y = i*0.5*tile_h + j*0.5*tile_h + tile_h*1.7 + offset
+    return (round(x), round(y))
 
 def pos_to_iso(pos): 
     if pos == -1:
@@ -49,7 +50,7 @@ def draw_camel(color, pos, height=0):
         pos = (pos + 1) % 16 # Offset starting position
     
     flip = "_flip" if pos >= 8 else ""
-    camel_sprite = Image.open(f"graphics/camel_{color.lower()}{flip}.png")
+    camel_sprite = Image.open(f"graphics/camel16x16_{color}.png")
     i, j = pos_to_iso(pos)
     if (pos >= 0 and pos < 4) or pos >= 12:
         canvas.alpha_composite(ImageOps.mirror(camel_sprite), iso_camel(i, j, height))
@@ -57,12 +58,14 @@ def draw_camel(color, pos, height=0):
         canvas.alpha_composite(camel_sprite, iso_camel(i, j, height))
 
 def draw_mod(mod_type, pos):
+    if pos != 0:
+        pos = (pos + 1) % 16 # Offset starting position
     orientation = "_up" if pos >= 8 else "_down" 
-    mod_sprite = Image.open(f"graphics/{mod_type}{orientation}.png")
+    mod_sprite = Image.open(f"graphics/{mod_type}{orientation}_16x16.png")
     width, height = mod_sprite.size
 
-    scale_factor = 3
-    mod_sprite = mod_sprite.resize((width * scale_factor, height * scale_factor), resample=Image.Resampling.BOX)
+    scale_factor = 1
+    mod_sprite = mod_sprite.resize((round(width * scale_factor), round(height * scale_factor)), resample=Image.Resampling.BOX)
     i, j = pos_to_iso(pos)
 
     if (pos >= 0 and pos < 4) or pos >= 12:
@@ -72,12 +75,24 @@ def draw_mod(mod_type, pos):
 
 
 draw_board(tile)
-for i in range(16):
-    draw_mod("trap", i)
 
 
+draw_camel("blue", 14, 2)
 
+draw_camel("green", 14, 1)
+draw_camel("yellow", 14, 0 )
 
-# image.alpha_composite(tile, tile_iso(0, 0))
+draw_camel("white", 16, 1)
+draw_camel("orange", 16)
+
+draw_mod("boost", 15)
+draw_mod("boost", 13)
+draw_mod("boost", 1)
+
+# for i in range(16):
+#     draw_mod("boost", i)
+
+sf = 4
+canvas = canvas.resize((screen_w * sf, screen_h * sf), resample=Image.Resampling.BOX)
 
 canvas.show()
